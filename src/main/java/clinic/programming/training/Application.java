f2bd5bebf60d680046fb73b74930d6e630281e97
root <root@LAPTOP-94Q65G6M.localdomain>
2025-01-16 07:13:10 +0000

    added stringutils dependencies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,8 +1,12 @@
 package clinic.programming.training;
 
 import java.util.*;
+import org.apache.commons.lang3.StringUtils;
 public class Application {
-    
+    	public int countWords(String words){
+		String[] sw=StringUtils.split(words,' ');
+		return (sw == null) ? 0 : sw.length;
+	}
 	public void greet() {
 		List<String> greetings = new ArrayList<>();
 		greetings.add("Hello");
@@ -19,5 +23,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
 	app.greet();
+	int c=app.countWords("Hello again stretford end");
+	System.out.println("Count" + "  "+c);
     }
 }
